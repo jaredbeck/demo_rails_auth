586b111f35537651bb6c7882ad1f3cad8e47eafa
Jared Beck <jared@jaredbeck.com>
2017-10-02 17:05:23 -0400

    Sign out UI, require auth. via callback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :require_authentication
+
+  private
+
+  def require_authentication
+    if session[:account_id].nil?
+      redirect_to new_authentication_path, alert: "Authentication required"
+    end
+  end
 end
--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,12 +1,21 @@
 class AuthenticationsController < ApplicationController
+  skip_before_action :require_authentication
+
   def create
     act = Account.find_by(email: creation_params.fetch(:email))
     if act&.authenticate(creation_params.fetch(:password))
+      session[:account_id] = act.id
       redirect_to(act, notice: "Authentication successful")
     else
       flash[:alert] = "Authentication failed"
       render :new
     end
+  end
+
+  def destroy
+    session[:account_id] = nil
+    flash[:notice] = "You are signed out. See you next time!"
+    render :new
   end
 
   def new
